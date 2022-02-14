8fb5b5489757a33849c7b99371587dc68faaedac
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-14 15:17:35 -0500

    Feat: create test and feat for professor_count in model

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,9 @@ class Student <ApplicationRecord
 
   has_many :professor_students
   has_many :professors, through: :professor_students
+
+
+  def professor_count
+    self.professors.count
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe 'methods' do
+    before (:each) do
+      @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+      ProfessorStudent.create(student_id: @harry.id, professor_id: @hagarid.id)
+    end
+    it 'counts teachers' do
+      expect(@harry.professor_count).to be(2)
+    end
+  end
 end
