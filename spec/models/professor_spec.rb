fe91f3f32b8d729e340f8a5205ffe7ebb8813852
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-14 15:32:30 -0500

    Refactor: move test to professor model

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  
+  describe 'methods' do
+    before (:each) do
+      @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+      ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+      ProfessorStudent.create(student_id: @malfoy.id, professor_id: @snape.id)
+      ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
+    end
+
+    it 'gives average age of students per professor' do
+      expect(@snape.avg_student_age).to eq(11)
+    end
+  end
 end
